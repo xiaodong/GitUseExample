166fbc9c9f04fbd67471ef8d33094f16c99f4fc1
Xander <siao@XanderMINI.local>
2013-02-02 00:00:56 +0800

    added a log message on app start, and a button with another log message.

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -1,4 +1,4 @@
-//
+//;
 //  ViewController.m
 //  GitUseExample
 //
@@ -24,6 +24,11 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickTheButton:(id)sender
+{
+    NSLog(@"This is a Git tutorial");
 }
 
 @end
