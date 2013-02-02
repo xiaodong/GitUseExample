6e92992ae1066f4f8a5c0af1320a71b4abc69b43
Xander <siao@XanderMINI.local>
2013-02-02 13:45:19 +0800

    new map feature

---
+++ GitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by Xander on 02/02/2013.
+//  Copyright (c) 2013 iDroidStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapForIternerary : UIViewController
+
+@end
---
+++ GitUseExample/MapForIternerary.m
@@ -0,0 +1,38 @@
+//
+//  MapForIternerary.m
+//  GitUseExample
+//
+//  Created by Xander on 02/02/2013.
+//  Copyright (c) 2013 iDroidStudio. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@interface MapForIternerary ()
+
+@end
+
+@implementation MapForIternerary
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
